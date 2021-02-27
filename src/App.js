ce996129b5a4a40740af7293598ad6946ef6e028
marksverdhei <marksverdhei@hotmail.com>
2021-02-27 14:08:01 +0100

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={"picardia.png"} className="App-logo" alt="logo" />
+        <p>
+          Stand by, more to come
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
