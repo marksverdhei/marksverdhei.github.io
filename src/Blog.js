70df4d64f4f2b7c11ba8539af086fdb0fc9f034d
marksverdhei <marksverdhei@hotmail.com>
2021-03-11 14:31:39 +0100

    Remove archives

--- src/Blog.js
+++ src/Blog.js
@@ -24,9 +24,6 @@ const useStyles = makeStyles((theme) => ({
 
 const sections = [
   { title: 'About Me', url: '#' },
-  { title: 'About Me', url: '#' },
-  { title: 'About Me', url: '#' },
-
   { title: 'Portfolio', url: '#' },
 ];
 
@@ -34,7 +31,7 @@ const mainFeaturedPost = {
   title: 'Title of a longer featured blog post',
   description:
     "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-  image: 'https://source.unsplash.com/random',
+  image: 'picardia.png',
   imgText: 'main image description',
   linkText: 'Continue reading…',
 };
@@ -45,7 +42,7 @@ const featuredPosts = [
     date: 'Nov 12',
     description:
       'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
+    image: 'tord.png',
     imageText: 'Image Text',
   },
   {
@@ -53,17 +50,15 @@ const featuredPosts = [
     date: 'Nov 11',
     description:
       'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
+    image: 'picardia.png',
     imageText: 'Image Text',
   },
 ];
 
 const posts = [
     post1,
-    // post2,
-    // post3
 ];
-console.log(posts);
+
 const sidebar = {
   title: 'Useless sidebar',
   description:
@@ -84,7 +79,7 @@ export default function Blog() {
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Header title="Blog" sections={sections} />
+        <Header title="Markus Heiervang" sections={sections} />
         <main>
           <MainFeaturedPost post={mainFeaturedPost} />
           <Grid container spacing={4}>
@@ -93,7 +88,7 @@ export default function Blog() {
             ))}
           </Grid>
           <Grid container spacing={5} className={classes.mainGrid}>
-            <Main title="From the firehose" posts={posts} />
+            <Main title="Latest posts" posts={posts} />
             <Sidebar
               title={sidebar.title}
               description={sidebar.description}
@@ -103,7 +98,7 @@ export default function Blog() {
           </Grid>
         </main>
       </Container>
-      <Footer title="Footer" description="Something here to give the footer a purpose!" />
+      <Footer title="What should I put here?" description="I have no clue!" />
     </React.Fragment>
   );
 }
