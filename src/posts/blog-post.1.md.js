d745f4b8df53e5481fa2724beb4b2f6902437da1
marksverdhei <marksverdhei@hotmail.com>
2021-03-11 14:30:55 +0100

    Change content of blog post

--- src/posts/blog-post.1.md.js
+++ src/posts/blog-post.1.md.js
@@ -1,12 +1,7 @@
 export default `
-# Sample blog post
+# The launch of my Machine Learning blog  
 
-#### April 1, 2020 by [Markus S. H.](/)
-I think I deserve a break now
-
-\`\`\`python
-class Foo:
-    def __init__(doesthiswork):
-        pass
-\`\`\`
+So this kind of blog page is basically just an example template created by
+the materialUI team. So of course I'll utilize it. I spend so much time on my studies
+so it would be a waste of time to try make something from scratch.
 `
